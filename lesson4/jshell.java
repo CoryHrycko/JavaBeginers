4ebb914fee58fac9b5b1331fd432ebd84ccc4520
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-12-12 19:51:21 -0500

    hustling away on that java

---
+++ lesson4/jshell.java
@@ -0,0 +1,6 @@
+String s1="Hello from JShell";
+System.out.println(s1);
+System.out.println(s1)
+int il =5;
+int i2 =10;
+int result = i1 + i2;
---
+++ lesson4/src/com/example/java/Main.java
@@ -0,0 +1,15 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello from Java!");
+        String s1="Hello from JShell";
+        System.out.println(s1);
+        int i1 =5;
+        int i2 =10;
+        int result = i1 + i2;
+        System.out.println("The answer is " + result);
+    }
+}
